a5055a5883214d419f859bdc1a7b36740b8938e1
SheepTester <ussa.say@gmail.com>
2021-06-27 21:24:42 -0700

    Add help command and aliases for existing commands
    Also revamp how handlers are done

--- index.js
+++ index.js
@@ -4,23 +4,92 @@ dotenv.config()
 import { Client } from 'discord.js'
 import 'discord-reply'
 import fsExtra from 'fs-extra'
+
 import { parseCommand } from './src/utils/parseCommand.js'
-import { ready, handlers } from './src/handlers.js'
+import { select } from './src/utils/select.js'
+import * as pollReactions from './src/poll-reactions.js'
+
+async function help (message) {
+  const aliases = new Map()
+  for (const [commandName, commandFunc] of Object.entries(commands)) {
+    if (!aliases.has(commandFunc)) {
+      aliases.set(commandFunc, new Set())
+    }
+    aliases.get(commandFunc).add(commandName)
+  }
+  message.lineReply(select([
+    'here you gooo',
+    'taste and sample as you please',
+    'please read carefully',
+    'the aliases sometimes describe what the command does, sometimes',
+    'helped'
+  ]), {
+    embed: {
+      title: select([
+        'nice, some commands',
+        'commands and aliases',
+        'words that i will accept',
+        'helpp'
+      ]),
+      fields: Array.from(aliases.values(), ([name, ...aliases]) => ({
+        name,
+        value: aliases.length
+          ? `or ${aliases.map(alias => `\`${alias}\``).join(' or ')}`
+          : select([
+            'no aliases, nice',
+            'that\'s it',
+            'this command has no aliases'
+          ]),
+        inline: true
+      }))
+    }
+  })
+}
+const commands = {
+  'poll channel': pollReactions.pollChannel,
+  'this is poll': pollReactions.pollChannel,
+  'this is poll channel': pollReactions.pollChannel,
+  'this is a poll channel': pollReactions.pollChannel,
+  'turn on poll channel mode which ': pollReactions.pollChannel,
+
+  'not poll channel': pollReactions.notPollChannel,
+  'not poll': pollReactions.notPollChannel,
+  'this is not a poll': pollReactions.notPollChannel,
+  'this is not a poll channel': pollReactions.notPollChannel,
+  'this isn\'t a poll channel': pollReactions.notPollChannel,
+  'turn off poll channel mode': pollReactions.notPollChannel,
+
+  'help': help,
+  'list all of the commands and their aliases': help
+}
 
 const client = new Client()
 
 client.on('message', async message => {
+  if (message.author.bot) return
+
   const parsed = parseCommand(message)
-  const data = { parsed, message, client }
-  for (const handler of handlers) {
-    if (await handler(data)) {
-      return
+  // If ping
+  if (parsed !== null) {
+    if (commands[parsed]) {
+      await commands[parsed](message)
+    } else {
+      await message.lineReply(select([
+        '<:ping:719277539113041930>',
+        'please do not needlessly ping me',
+        'do you need help? reply to this message with `help`'
+      ]))
     }
+    return
   }
+
+  pollReactions.onMessage(message)
 })
 
 fsExtra.ensureDir('./data/')
-  .then(() => ready())
+  .then(() => Promise.all([
+    pollReactions.onReady()
+  ]))
   .then(() => {
     client.login(process.env.TOKEN)
   })
--- src/poll-reactions.js
+++ src/poll-reactions.js
@@ -11,31 +11,33 @@ function isPollChannel (message) {
 
 const ok = ['👌', '🆗', '👍', '✅']
 
-export async function handleMessage ({ parsed, message }) {
-  if (parsed === 'poll channel') {
-    if (isPollChannel(message)) {
-      await message.lineReply(select([
-        'this is already a poll channel though',
-        'didn\'t you already do `poll channel`',
-        'that doesn\'t do anything if this channel already is a poll channel'
-      ]))
-    } else {
-      pollChannels.set(message.channel.id, true).save()
-      await message.react(select(ok))
-    }
-    return true
-  } else if (parsed === 'not poll channel') {
-    if (isPollChannel(message)) {
-      pollChannels.set(message.channel.id, false).save()
-      await message.react(select(ok))
-    } else {
-      await message.lineReply(select([
-        'this isn\'t a poll channel though',
-        'that doesn\'t do anything if this channel already isn\'t a poll channel'
-      ]))
-    }
-    return true
+export async function pollChannel (message) {
+  if (isPollChannel(message)) {
+    await message.lineReply(select([
+      'this is already a poll channel though',
+      'didn\'t you already do `poll channel`',
+      'that doesn\'t do anything if this channel already is a poll channel'
+    ]))
+  } else {
+    pollChannels.set(message.channel.id, true).save()
+    await message.react(select(ok))
   }
+}
+
+export async function notPollChannel (message) {
+  if (isPollChannel(message)) {
+    pollChannels.set(message.channel.id, false).save()
+    await message.react(select(ok))
+  } else {
+    await message.lineReply(select([
+      'this isn\'t a poll channel though',
+      'that doesn\'t do anything if this channel already isn\'t a poll channel'
+    ]))
+  }
+  return true
+}
+
+export async function onMessage (message) {
   if (isPollChannel(message)) {
     console.log(message.content)
   }
