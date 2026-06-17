78f9b39fbaefd747c27d91703cd31f191bc2cad4
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-06-16 21:46:22 -0700

    idk why i deleted the build command, also soft update probably won't be needed anymore

--- src/commands/internals.ts
+++ src/commands/internals.ts
@@ -47,6 +47,7 @@ async function prepareUpdate (
   await reportExec('git pull')
   await reportExec('npm ci')
   await reportExec('npx playwright install firefox')
+  await reportExec('npm run build')
   await reportExec('pm2 reload floofy-bot')
 }
 
@@ -59,29 +60,7 @@ export async function exit (message: Message): Promise<void> {
 
     console.log('Restarting')
     await prepareUpdate(msg, results)
-    results.push('Exiting...')
-    await msg.edit(displayResults(results))
-    process.exit()
-  } else {
-    await message.reply(
-      select([
-        'shoo',
-        `you are not <@${process.env.OWNER}>`,
-        'out of here commoner',
-        'scram plebian'
-      ])
-    )
-  }
-}
-
-export async function softUpdate (message: Message): Promise<void> {
-  if (message.author.id === process.env.OWNER) {
-    const msg = await message.reply(select(['mmk', 'okie dokie', 'aight']))
-    const results: Results = []
-
-    console.log('Soft updating')
-    await prepareUpdate(msg, results)
-    results.push('Done')
+    results.push('Done...?')
     await msg.edit(displayResults(results))
   } else {
     await message.reply(
