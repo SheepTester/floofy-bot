c703f1d5c70ccb88f57db065dc61110339217f1a
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-06-17 21:48:10 -0700

    Copy dbmate migrations from web-server-2

---
+++ db/migrations/20260617063358_mentions.sql
@@ -0,0 +1,13 @@
+-- migrate:up
+create table mentions (
+  channel_id text not null,
+  mentioned text not null,
+  author text not null,
+  content text not null,
+  message_url text not null,
+  is_role boolean not null,
+  primary key (channel_id, mentioned)
+);
+
+-- migrate:down
+drop table if exists mentions;
---
+++ db/migrations/20260617063802_wise-guy.sql
@@ -0,0 +1,14 @@
+-- migrate:up
+create table wise_guy (
+  guild_id text not null,
+  last_time integer not null default 0,
+  last_channel_id text,
+  last_message text not null default '',
+  -- JSON array because I'm lazy
+  replies text not null default '[]',
+  guild_frequency integer,
+  primary key (guild_id)
+);
+
+-- migrate:down
+drop table if exists wise_guy;
